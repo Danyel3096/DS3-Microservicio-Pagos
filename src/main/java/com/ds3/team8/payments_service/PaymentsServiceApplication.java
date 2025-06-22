41a3c1ea8b65552b560d3b8309c627bfe275c549
JuanLopezAranzazu <juanlopezaranzazu@gmail.com>
2025-06-22 16:06:38 -0500

    Estructura del microservicio

---
+++ src/main/java/com/ds3/team8/payments_service/PaymentsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ds3.team8.payments_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsServiceApplication.class, args);
+	}
+
+}
