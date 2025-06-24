f4ce3f59beda1c287e45a3f822986a80799310cf
JuanLopezAranzazu <juanlopezaranzazu@gmail.com>
2025-06-23 23:01:10 -0500

    Feat: Enable Feign clients in PaymentsServiceApplication for enhanced service communication

--- src/main/java/com/ds3/team8/payments_service/PaymentsServiceApplication.java
+++ src/main/java/com/ds3/team8/payments_service/PaymentsServiceApplication.java
@@ -2,8 +2,10 @@ package com.ds3.team8.payments_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PaymentsServiceApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class PaymentsServiceApplication {
 	}
 
 }
+
